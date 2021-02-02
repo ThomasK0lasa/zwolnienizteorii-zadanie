75b49f79c0cfa5eb383f89070f2f80a985a67946
Kolasa Thomas <kolasa.at.work@gmail.com>
2021-02-02 21:09:19 +0100

    feat: add API login support

--- src/app.js
+++ src/app.js
@@ -4,39 +4,81 @@ import './styles/pages.scss';
 import './styles/form.scss';
 import './styles/buttons.scss';
 
-let username = null;
+const apiUrl = "https://zwzt-zadanie.netlify.app/api/login";
 
-route('/', 'home.ejs', function() {
+route('/', 'home.ejs', function () {
   this.errorMessage = null;
-  this.username = username;
-  this.$on('.login-button', 'click', () => {
-    event.preventDefault();
-    username = document.getElementById('input-username').value;
-    console.log(document.getElementById('input-password').value);
-    redirect('/success');
-    /*this.errorMessage = 'Error!';
-    this.$refresh();*/
+  this.username = localStorage.getItem('username');
+  this.$on('.login-button', 'click', async () => {
+    const username = document.getElementById('input-username').value;
+    const password = document.getElementById('input-password').value;
+    const response = await login({ username: username, password: password });
+    if (!response.error) {
+      localStorage.setItem('username', username);
+      localStorage.setItem('token', response.token);
+      redirect('/success');
+    } else {
+      this.errorMessage = response.message;
+      this.$refresh();
+    }
+  });
+  this.$on('.logout-button', 'click', () => {
+    this.username = null;
+    logout();
+    this.$refresh();
   });
 });
 
-route('/success', 'success.ejs', function() {
-  if (!username) {
+route('/success', 'success.ejs', function () {
+  this.username = null;
+  if (!isAuthorized()) {
     redirect('/unauthorized');
     return;
   }
-  this.username = username;
+  this.username = localStorage.getItem('username');;
   this.$on('.logout-button', 'click', () => {
-    username = null;
+    logout();
     redirect('/');
   });
 });
 
-route('/unauthorized', 'unauthorized.ejs', function() {});
+route('/unauthorized', 'unauthorized.ejs', function () { });
 
-route('/about', 'about.ejs', function() {});
+route('/about', 'about.ejs', function () { });
 
-route('*', '404.ejs', function () {});
+route('*', '404.ejs', function () { });
 
 function redirect(path) {
-  window.location.hash = '#'+path;
+  window.location.hash = '#' + path;
+}
+
+async function login(data = {}) {
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    return response.json();
+  } catch (error) {
+    return {error: true, message: error};
+  }
+}
+
+function logout() {
+  localStorage.removeItem('username');
+  localStorage.removeItem('token');
+}
+
+function isAuthorized() {
+  // this is not safe solution
+  // token should be validated on the server
+  return !!localStorage.getItem('token');
+}
+
+function validator() {
+
 }
